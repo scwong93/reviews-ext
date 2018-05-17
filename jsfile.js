46b0308c0340f8d82161cfc5676dabf12aceea22
Simon <Simon@Simons-MacBook-Air.local>
2018-05-17 16:30:59 -0700

    refactor code, add walmart api

--- jsfile.js
+++ jsfile.js
@@ -1,7 +1,19 @@
-fetch("https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js")
+fetch("http://api.walmartlabs.com/v1/search?apiKey=vck5ypdbw4jnhuceesmjjce5&query=NETGEAR")
 .then(function (response) {
+  console.log(response);
     response.text().then(function (responseText) {
         console.log(responseText);
         alert("Done using Fetch!");
     });
-});
+})
+
+
+// get item name off screen
+
+// query walmart for product id - https://developer.walmartlabs.com/docs/read/Search_API
+
+// use product id to find reviews
+
+// get ul off screen by id = Reviews
+
+// loop over reviews and create li for each and append to ul
