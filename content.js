d18a84b5e54ad69416c0d92f0d00e6444abf92d1
Simon <Simon@Simons-MacBook-Air.local>
2018-05-21 15:13:05 -0700

    refactor code

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-fetch("https://api.walmartlabs.com/v1/search?apiKey=vck5ypdbw4jnhuceesmjjce5&query=NETGEAR")
+fetch("https://api.walmartlabs.com/v1/search?apiKey=vck5ypdbw4jnhuceesmjjce5&query=NETGEAR&format=json&callback=foo")
 .then(function (response) {
   console.log(response);
     response.text().then(function (responseText) {
@@ -7,24 +7,26 @@ fetch("https://api.walmartlabs.com/v1/search?apiKey=vck5ypdbw4jnhuceesmjjce5&que
     });
 }).then(function (responseText) {
   if (responseText) {
-    let product = responseText[0];
-    let itemId = product.itemId;
+    var product = responseText[0];
+    console.log(product)
+    var itemId = product.itemId;
     console.log(itemId);
-  }
-});
-fetch("https://api.walmartlabs.com/v1/reviews/{itemId}?apiKey=vck5ypdbw4jnhuceesmjjce5&format=json")
-.then(function (response) {
-  console.log(response);
-    response.text().then(function (responseText) {
-      console.log(responseText);
+    fetch(`https://api.walmartlabs.com/v1/reviews/${itemId}?apiKey=vck5ypdbw4jnhuceesmjjce5&format=json&callback=foo`)
+    .then(function (response) {
+        response.text().then(function (responseText) {
+        });
+    }).then(function (responseText) {
+      var reviews = document.getElementById('reviews');
+      var walmartReviews = responseText.reviewText
+      for (i in walmartReviews) {
+        let li = document.createElement('li');
+        li.innerHTML = walmartsReviews[i];
+        reviews.append(li);
+      }
     });
-}).then(function (responseText) {
-  let reviews = document.getElementById('reviews');
-  let walmartReviews = responseText.reviews
-  for (i in walmartReviews) {
-    reviews.append(walmartReviews[i]);
   }
 });
+
 
 
 
