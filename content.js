c18114bd18fb23890c68c7cea9dcea652a57a773
Simon <Simon@Simons-MacBook-Air.local>
2018-06-11 12:35:08 -0700

    removed background script

--- content.js
+++ content.js
@@ -7,29 +7,15 @@ fetch("https://api.walmartlabs.com/v1/search?apiKey=vck5ypdbw4jnhuceesmjjce5&que
     var itemId = product.itemId;
     fetch(`https://api.walmartlabs.com/v1/reviews/${itemId}?apiKey=vck5ypdbw4jnhuceesmjjce5&format=json`)
     .then(function (response) {
-        return response.json();
+      return response.json();
     }).then(function (responseText) {
       var reviews = document.getElementById('reviews');
-      console.log(reviews);
       var walmartReviews = responseText.reviews
       for (i in walmartReviews) {
-        let li = document.createElement('li');
+        var li = document.createElement('li');
         li.innerHTML = walmartReviews[i].reviewText;
         reviews.append(li);
       }
     });
   }
 });
-
-
-
-
-// get item name off screen
-
-// query walmart for product id - https://developer.walmartlabs.com/docs/read/Search_API
-
-// use product id to find reviews
-
-// get ul off screen by id = Reviews
-
-// loop over reviews and create li for each and append to ul
