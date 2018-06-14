cffcacc666c2af0112b86e06c5c5d35ba0f0339d
Simon <Simon@Simons-MacBook-Air.local>
2018-06-14 13:14:57 -0700

    generated reviews to popup.html

--- popup.js
+++ popup.js
@@ -1,9 +1,28 @@
 'use strict';
 
-let getReview = document.getElementById('reviews-btn');
+let getItems = document.getElementById('reviews-btn');
+let productTitle = document.getElementById('productTitle');
 
-getReview.onclick = function(element) {
-  chrome.tabs.executeScript({
-    file: "content.js"
-  });
-};
+getItems.onclick = () => {
+  fetch("https://api.walmartlabs.com/v1/search?apiKey=vck5ypdbw4jnhuceesmjjce5&query=NETGEAR&format=json")
+    .then(function (response) {
+      return response.json();
+    }).then(function (responseText) {
+      if (responseText) {
+        var product = responseText.items[0];
+        var itemId = product.itemId;
+        fetch(`https://api.walmartlabs.com/v1/reviews/${itemId}?apiKey=vck5ypdbw4jnhuceesmjjce5&format=json`)
+          .then(function (response) {
+            return response.json();
+          }).then(function (responseText) {
+            var reviews = document.getElementById('reviews');
+            var walmartReviews = responseText.reviews;
+            walmartReviews.map(review => {
+              var li = document.createElement('li');
+              li.innerHTML = review.reviewText;
+              reviews.append(li);
+            })
+          });
+      }
+    });
+}
