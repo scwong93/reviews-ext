b5983485efeb1ef35bb96af91a40201a2dc0d489
Simon <scwong93@gmail.com>
2018-07-01 10:00:54 -0700

    removed redundant code in content

--- content.js
+++ content.js
@@ -1,10 +1,3 @@
-
-
-/*chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  var activeTab = tabs[0];
-  chrome.tabs.sendMessage(activeTab.id, {"message": productTitle});
-});*/
-
 var extensionId = "kcpllmimpckifppeknmiakaiaabllkcg";
 var productTitle = document.getElementById('productTitle').innerHTML.trim();
 chrome.runtime.sendMessage(extensionId, {"message": productTitle});
